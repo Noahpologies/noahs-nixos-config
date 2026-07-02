47ad6357e4a178795e7469cc3e6015729dcb0a95
Noah Saunders <nsaunders5520@gmail.com>
2026-07-02 18:33:24 -0400

    made alias for bsdgames

--- games.nix
+++ games.nix
@@ -12,6 +12,13 @@
   enable = true;
   remotePlay.openFirewall = true;
   dedicatedServer.openFirewall = true;
+};
+
+programs.bash.shellAliases = {
+
+  # not sure where to put this but here's a list of the games in bsdgames
+
+  bsdgames = "echo 'bsdgames came with all of these: Adventure, arithmetic, atc, backgammon, banner-bsd, battlestar, bcd, boggle, caesar, canfield, cfscores, countmail, cribbage, factor-bsd, fish, fortune, gomoku, hangman, hunt, huntd, mille, monop, morse, number, pig, pom, ppt, primes, quiz, rain, random, robots, rot13, snake, snscore, strfile, teachgammon, tetris-bsd, trek, wargames, worm-bsd, worms, wtf, wump' | lolcat | cowsay";
 
 };
 
