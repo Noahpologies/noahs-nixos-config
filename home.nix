64260673227ddb5fec0e0761131fbe8616f9711f
Noah Saunders <nsaunders5520@gmail.com>
2026-07-05 14:02:00 -0400

    aliases

--- home.nix
+++ home.nix
@@ -11,4 +11,12 @@
 
     stateVersion = "26.05";
   };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#erasmus";
+      rebuild-upgrade = "nix flake update --flake /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos#eramsus";
+    };
+  };
 }
