df33b6d00bcb014d904339a4ec48affe7ed0f78c
Noah Saunders <nsaunders5520@gmail.com>
2026-07-08 09:05:55 -0400

    wallpaper

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 # flake.nix
 {
-  description = "NixOS + Home Manager configuration";
+  description = "NixOS with Home Manager configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
@@ -8,20 +8,26 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, plasma-manager, ... }:
     {
       nixosConfigurations.erasmus = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.users.noah = import ./home.nix;
+            home-manager.sharedModules = [ plasma-manager.homeModules.plasma-manager ];
           }
         ];
       };
--- home.nix
+++ home.nix
@@ -19,4 +19,35 @@
       rebuild-upgrade = "nix flake update --flake /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos#eramsus";
     };
   };
+
+  xdg.dataFile = {
+    "plasma/desktoptheme/daemon" = {
+      source = ./dotfiles/daemon/plasma-styles;
+      recursive = true;
+    };
+    "color-schemes" = {
+      source = ./dotfiles/daemon/colors;
+      recursive = true;
+    };
+    "aurorae/themes/daemon" = {
+      source = ./dotfiles/daemon/window-borders;
+      recursive = true;
+    };
+    "icons/Daemon" = {
+      source = ./dotfiles/daemon/icons/Daemon;
+      recursive = true;
+    };
+  };
+
+  xdg.configFile."Kvantum" = {
+    source = ./dotfiles/daemon/kvantum-themes;
+    recursive = true;
+  };
+
+  home.file.".config/BetterDiscord/themes" = {
+    source = ./dotfiles/daemon/discord;
+    recursive = true;
+  };
+
+  programs.plasma.workspace.wallpaper = ./dotfiles/daemon/wallpaper/Editedcyberwall.png;
 }
