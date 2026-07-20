395e06d7d19bcfc6f2dcdcc9e3bc71bdf40cd34e
Noah Saunders <nsaunders5520@gmail.com>
2026-07-19 20:37:34 -0400

    pls work claude

--- dotfiles/daemon/daemon-imports.nix
+++ dotfiles/daemon/daemon-imports.nix
@@ -1,10 +1,9 @@
 # Daemon imports
-
 { lib, pkgs, ... }:
 
-    {
-    xdg.dataFile = {
-    "plasma/desktoptheme/daemon" = {
+{
+  xdg.dataFile = {
+    "plasma/desktoptheme" = {
       source = ./plasma-styles;
       recursive = true;
     };
@@ -12,7 +11,7 @@
       source = ./colors;
       recursive = true;
     };
-    "aurorae/themes/daemon" = {
+    "aurorae/themes" = {
       source = ./window-borders;
       recursive = true;
     };
@@ -20,19 +19,14 @@
       source = ./icons/Daemon;
       recursive = true;
     };
-
-    # Splash screen
     "plasma/look-and-feel/Daemon" = {
       source = ./splash-screen/Daemon;
       recursive = true;
     };
-
-    # Konsole color scheme
     "konsole" = {
       source = ./konsole-colors;
       recursive = true;
     };
-
     "org.kde.syntax-highlighting/theme" = {
       source = ./kate-colors;
       recursive = true;
@@ -44,28 +38,49 @@
     recursive = true;
   };
 
-#  home.file.".config/BetterDiscord/themes" = {
-#    source = ./discord;
-#    recursive = true;
-#  };
-
-  # Font
   home.file.".local/share/fonts/Prototype.ttf".source = ./fonts/Prototype.ttf;
   fonts.fontconfig.enable = true;
 
-  # Cursor theme
   home.packages = with pkgs; [
     bibata-cursors
   ];
+
+  qt = {
+    enable = true;
+    platformTheme.name = "kde";
+    style.name = "kvantum";
+  };
 
   programs.plasma = {
     enable = true;
 
     workspace = {
       wallpaper = ./wallpapers/Editedcyberwall.png;
-      lookAndFeel = "org.kde.plasma.daemon";
       splashScreen = { theme = "Daemon"; };
       cursor.theme = "Bibata-Original-Classic";
+      theme = "Daemon";
+      colorScheme = "Daemon";
+      iconTheme = "Daemon";
+    };
+
+    fonts = {
+      general = { family = "Prototype"; pointSize = 10; };
+      menu = { family = "Prototype"; pointSize = 10; };
+      toolbar = { family = "Prototype"; pointSize = 10; };
+    };
+
+    configFile."kwinrc"."org.kde.kdecoration2" = {
+      theme = "__aurorae__svg__Daemon";
+      BorderSize = "VeryLarge";
+      BorderSizeAuto = false;
+    };
+
+    configFile."kwinrc"."Plugins" = {
+      kwin6_effect_hexagonEnabled = true;
+      cubeEnabled = true;
+      eyeonscreenEnabled = true;
+      mousemarkEnabled = true;
+      translucencyEnabled = true;
     };
   };
 }
