82389d1d5d8ab22b56ec87933a10dee472f78859
Noah Saunders <nsaunders5520@gmail.com>
2026-09-06 21:37:51 -0400

    using my calculator button for something

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -26,6 +26,7 @@
 	"$mod, S, exec, steam"
 	"$mod, F, exec, firefox"
 	"$mod, o, exec, obsidian"
+	
 
 	# general
 	"$mod, W, exec, waypaper"
@@ -73,8 +74,9 @@
           ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
           ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
           ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-	    ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
-            ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+	  ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
+          ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+	  ", XF84Calculator, exec, firefox ~/software/desmos-offline/desmos.html"
         ];
 
       animations = {
